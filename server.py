72881eeb3c841cd84e170eaa5a137eef315c1553
Paulo Ricardo Viviurka do Carmo <31288239+PauloRVdC@users.noreply.github.com>
2021-01-13 16:42:17 -0300

    open html to redirect

--- server.py
+++ server.py
@@ -1,5 +1,10 @@
 from os import environ
 from flask import Flask
 
-app = Flask(__name__)
-app.run(environ.get('PORT'))
+@app.route("/")
+def index():
+  return "index.html"
+
+if __name__ == "__main__":
+  app = Flask(__name__)
+  app.run(environ.get('PORT'))
