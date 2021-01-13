3d41c58cbd3851d3c4f666433d8757c3736d9bf7
Paulo Ricardo Viviurka do Carmo <31288239+PauloRVdC@users.noreply.github.com>
2021-01-13 17:13:31 -0300

    Update server.py

--- server.py
+++ server.py
@@ -1,10 +1,5 @@
 from os import environ
 from flask import Flask
 
-@app.route("/")
-def index():
-  return "<meta http-equiv="refresh" content="time; URL=https://hourlyloonacatcher.carrd.co/" />"
-
-if __name__ == "__main__":
-  app = Flask(__name__)
-  app.run(environ.get('PORT'))
+app = Flask(__name__)
+app.run(environ.get('PORT'))
