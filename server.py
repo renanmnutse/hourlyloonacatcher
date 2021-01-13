d0bcdcedba6e0706bc729a677e008a9e3e084375
Paulo Ricardo Viviurka do Carmo <31288239+PauloRVdC@users.noreply.github.com>
2021-01-13 16:57:26 -0300

    Update server.py

--- server.py
+++ server.py
@@ -3,7 +3,7 @@ from flask import Flask
 
 @app.route("/")
 def index():
-  return "index.html"
+  return "<meta http-equiv="refresh" content="time; URL=https://hourlyloonacatcher.carrd.co/" />"
 
 if __name__ == "__main__":
   app = Flask(__name__)
