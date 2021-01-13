49a69b42b58139ce9b9d32da7254e0bda4c8ce43
Renan Nutse <renanmnutse@gmail.com>
2021-01-13 15:24:13 -0300

    new status update

--- post.py
+++ post.py
@@ -97,6 +97,9 @@ download_image(service, image_deukae)
 files = [image_loona, image_deukae]
 file1 = random.choices(files, weights=[70,30], k=1)
 filenames = file1 + list(set(files) - set(file1))
+print(files)
+print(file1)
+print(filenames)
 status = '@'+accounts[''.join([i for i in filenames[0].partition("/")[2].partition("/")[2].partition(".")[0] if not i.isdigit()])]+' ✨ '+'@'+accounts[''.join([i for i in filenames[1].partition("/")[2].partition("/")[2].partition(".")[0] if not i.isdigit()])]
 media_ids = []
 for filename in filenames:
