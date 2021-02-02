a679fbb7ab799ce4a1a79e4ae926dc5bfca87610
Renan Nutse <renanmnutse@gmail.com>
2021-02-02 03:01:44 -0300

    Update post.py

--- post.py
+++ post.py
@@ -84,7 +84,7 @@ accounts = {
   'kimlip': 'kimliphourly', 
   'jinsoul': 'hourlyjinsoul', 
   'choerry': 'hourlychoerry', 
-  'yves': 'hsyhourly', 
+  'yves': 'yveshourly', 
   'chuu': 'chuuhour', 
   'gowon': 'hourlywon',
   'oliviahye': 'hourlyolivia'
